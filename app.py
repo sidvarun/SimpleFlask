3a508ad923863228d3c97addf54fdf5255892e93
Varun Kumar <varunksidhwani@gmail.com>
2021-11-28 10:20:57 +0530

    req

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask,request, url_for, redirect, render_template
 import pandas as pd
 import pickle
-from flask_cors import CORS
+# from flask_cors import CORS
 app = Flask(__name__) 
-CORS(app)
+# CORS(app)
 
 
 model = pickle.load(open("example_weights_knn.pkl", "rb"))
